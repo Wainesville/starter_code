c2c2b1863a2432575824ef424177d33a8d3bc31e
wainesville <wainesville@gmail.com>
2024-05-06 14:12:28 -0700

    lowercase

--- script.js
+++ script.js
@@ -4,22 +4,26 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
+function lowerCased(fruit){
+const newFruit = [];
+return fruit
+.toLowerCase()
+.filter(function(val){
+	return newFruit.indexOf(val);
+})
+
+}
 
 function search(input) {
 	let results = [];
 	var newFruit = fruit.map(function(v) {
 		return v.toLowerCase();
 	  });
-	  var  searchVal = newFruit.map(function(v){
-		if(input.indexOf(newFruit) !== -1){
-			results.push(searchVal);
-		}
-	  });
-	  
-
-	  
-	//   console.log(newFruit);
-		
+for(var i=0;i<fruit.length;i++)
+	{
+		results.push(newFruit[i])
+	}
+	
 	return results;
 	
 }
@@ -33,7 +37,6 @@ function searchHandler(e) {
 
 
 function showSuggestions(results, inputVal) {
-
 console.log("test",results)
 }
 
